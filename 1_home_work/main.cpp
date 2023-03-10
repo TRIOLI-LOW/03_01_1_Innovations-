2f855a6eb48b23e2b17b07c1cb74885daf389e3d
TRIOLI LOW <trioli.low@gmail.com>
2023-03-10 23:05:21 +0300

    initial

--- 1_home_work/main.cpp
+++ 1_home_work/main.cpp
@@ -5,12 +5,8 @@
 int main() {
     std::vector<int> arr = { 4, 7, 9, 14, 12 };
 
-    std::for_each(arr.begin(), arr.end(), [](int& x) { if (x % 2 != 0) x *= 3; });
-
-    for (const auto& x : arr) {
-        std::cout << x << " ";
-    }
-    std::cout << std::endl;
-
+    std::for_each(arr.begin(), arr.end(), [](int& x) { 
+        if (x % 2 != 0) x *= 3; 
+            std::cout << x << " "; });
     return 0;
 }
